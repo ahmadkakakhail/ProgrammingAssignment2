45795a690d0cfb2f1e900a04cff7fcb1fcb67fc2
ahmadkakakhail <ahmadkakakhail@yahoo.com>
2020-06-12 02:11:19 +0500

    This file contains the implementation of Cache Matrix Inversion as per the requirement of the course "R Programming" on Coursera.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This is the Lexical Scoping assignment as per requirement of the course "R Programming" on Coursera
 
-## Write a short comment describing this function
+# The function "makeCacheMatrix" creates a special "matrix" object that has the ability to cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(inv) {
+    i <<- inv
+  }
+  
+  getInverse <- function() i
+  
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+# The function "cacheSolve" computes the inverse of special "matrix" created by "makeCacheMatrix"
+## If the inverse already exists, it will take the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Return a matrix that is the inverse of 'x'
+  
+  i <- x$getInverse()
+  
+  if(!is.null(i)) {
+    message("getting cached inverse")
+    return(i)
+  }
+  
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setInverse(i)
+  i
 }
